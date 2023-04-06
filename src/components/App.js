317cb052ace0cc2edc5884a32eb86119cff633ab
sunit seth <100702483+sunitseth1@users.noreply.github.com>
2023-04-06 22:33:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    let relatives = [
+      "Ritesh",
+      "Satyam",
+      "Harshit",
+      "Swarup",
+      "Abhishek",
+      "Rahul"
+    ];
+    return (
+      <div id="main">
+        <ol key="relativeList">
+          {relatives.map((relative, index) => (
+            <li key={`relativeListItem${index + 1}`}>{relative}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
